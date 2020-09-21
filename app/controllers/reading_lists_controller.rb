a43a708b15325696c88ece53fe817e3d8d3e9097
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-21 14:35:01 -0400

    Built controllers and views for index and show paths on reading list model

--- app/controllers/reading_lists_controller.rb
+++ app/controllers/reading_lists_controller.rb
@@ -1,2 +1,10 @@
 class ReadingListsController < ApplicationController
+
+  def index
+    @reading_lists = ReadingList.all
+  end
+
+  def show
+    @reading_list = ReadingList.find(params[:id])
+  end
 end
